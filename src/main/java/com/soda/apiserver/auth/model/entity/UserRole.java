19e968fc8a3d319876a343c6113e51ff2713ccc9
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-09-20 22:16:59 +0900

    feature: UserRole mapping table sequence 추가, UserRole id 추가

--- src/main/java/com/soda/apiserver/auth/model/entity/UserRole.java
+++ src/main/java/com/soda/apiserver/auth/model/entity/UserRole.java
@@ -5,23 +5,44 @@ import java.io.Serializable;
 
 @Entity
 @Table(name = "TBL_USER_ROLE")
+@SequenceGenerator(
+        name = "SEQ_USER_ROLE_ID_GENERATOR",
+        sequenceName = "SEQ_USER_ROLE_ID",
+        initialValue = 1,
+        allocationSize = 1
+)
 public class UserRole implements Serializable {
     @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "SEQ_USER_ROLE_ID_GENERATOR"
+    )
+    @Column(name = "USER_ROLE_ID")
+    private int UserRoleId;
+
     @ManyToOne
-    @JoinColumn(name="USER_ID")
+    @JoinColumn(name="USER_ID", referencedColumnName = "USER_ID")
     private User user;
 
-    @Id
     @ManyToOne
-    @JoinColumn(name = "ROLE_ID")
+    @JoinColumn(name = "ROLE_ID", referencedColumnName = "ROLE_ID")
     private Role role;
 
     public UserRole() {
     }
 
-    public UserRole(User user, Role role) {
+    public UserRole(int userRoleId, User user, Role role) {
+        UserRoleId = userRoleId;
         this.user = user;
         this.role = role;
+    }
+
+    public int getUserRoleId() {
+        return UserRoleId;
+    }
+
+    public void setUserRoleId(int userRoleId) {
+        UserRoleId = userRoleId;
     }
 
     public User getUser() {
@@ -43,8 +64,9 @@ public class UserRole implements Serializable {
     @Override
     public String toString() {
         return "UserRole{" +
-                "user=" + user +
-                ", role=" + role +
+                "UserRoleId='" + UserRoleId + '\'' +
+                ", user=" + user.getUserName() +
+                ", role=" + role.getRoleName() +
                 '}';
     }
 }
