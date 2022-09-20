6c9879f04db5894e1cc659e7a24abdd875f8c7ce
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-09-20 22:12:30 +0900

    feature: SignUpController ResponseEntity 적용 및 UserRole 추가

--- src/main/java/com/soda/apiserver/auth/controller/SignUpController.java
+++ src/main/java/com/soda/apiserver/auth/controller/SignUpController.java
@@ -1,16 +1,20 @@
 package com.soda.apiserver.auth.controller;
 
 import com.soda.apiserver.auth.model.dto.SignUpDTO;
+import com.soda.apiserver.auth.model.entity.Role;
 import com.soda.apiserver.auth.model.entity.User;
 import com.soda.apiserver.auth.model.entity.UserRole;
+import com.soda.apiserver.auth.repository.RoleRepository;
 import com.soda.apiserver.auth.repository.UserRepository;
+import com.soda.apiserver.auth.repository.UserRoleRepository;
 import com.soda.apiserver.common.response.ResponseMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,25 +27,28 @@ import java.util.*;
 @RestController
 public class SignUpController {
     private final UserRepository repository;
+    private final UserRoleRepository userRoleRepository;
+    private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public SignUpController(UserRepository repository, PasswordEncoder passwordEncoder) {
+    public SignUpController(UserRepository repository, UserRoleRepository userRoleRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
         this.repository = repository;
+        this.userRoleRepository = userRoleRepository;
+        this.roleRepository = roleRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping("/signup")
-    public ResponseEntity<?> signUp(@RequestBody SignUpDTO signUp, HttpServletResponse response){
+    public ResponseEntity<?> signUp(@RequestBody SignUpDTO signUp){
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(new MediaType("application","json", Charset.forName("UTF-8")));
         Map<String,Object> responseMap = new HashMap<>();
+        User user = null;
 
         try {
-            List<UserRole> userRole = new ArrayList<>();
-            userRole.add(new UserRole());
-            repository.save(new User(0,
+            user = repository.save(new User(0,
                     signUp.getUserName(),
                     passwordEncoder.encode(signUp.getPassword()),
                     signUp.getEmail(),
@@ -49,8 +56,13 @@ public class SignUpController {
                     "N",
                     null,
                     null,
-                    userRole
+                    null
             ));
+            System.out.println("user : "+user);
+            Role role = roleRepository.findByRoleName("MEMBER");
+
+            userRoleRepository.save(new UserRole(0,user,role));
+
         } catch (Exception e){
             System.out.println(e);
             responseMap.put("Input ID",signUp.getUserName());
@@ -61,8 +73,8 @@ public class SignUpController {
                     .body(new ResponseMessage(400, "user not created",responseMap));
         }
 
-        responseMap.put("ID",signUp.getUserName());
-        responseMap.put("Email",signUp.getEmail());
+        responseMap.put("ID",user.getUserName());
+        responseMap.put("Email",user.getEmail());
 
         return ResponseEntity
                 .created(URI.create("/users/"+signUp.getUserName()))
