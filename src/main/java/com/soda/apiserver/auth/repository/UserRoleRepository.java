83d4b5186693ad48c8d4619258f209d60df80706
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-09-21 12:36:51 +0900

    fix: remove untracked imports

--- src/main/java/com/soda/apiserver/auth/repository/UserRoleRepository.java
+++ src/main/java/com/soda/apiserver/auth/repository/UserRoleRepository.java
@@ -1,7 +1,6 @@
 package com.soda.apiserver.auth.repository;
 
 import com.soda.apiserver.auth.model.entity.UserRole;
-import com.soda.apiserver.auth.model.entity.embeded.UserRoleId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRoleRepository extends JpaRepository<UserRole, Integer> {
