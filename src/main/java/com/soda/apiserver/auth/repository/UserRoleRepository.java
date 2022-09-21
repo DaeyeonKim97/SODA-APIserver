36876dfaaa5d7bd8db8ba468b7fea4311380dce8
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-09-21 22:07:39 +0900

    feature: implement findByUser repository method

--- src/main/java/com/soda/apiserver/auth/repository/UserRoleRepository.java
+++ src/main/java/com/soda/apiserver/auth/repository/UserRoleRepository.java
@@ -1,7 +1,11 @@
 package com.soda.apiserver.auth.repository;
 
+import com.soda.apiserver.auth.model.entity.User;
 import com.soda.apiserver.auth.model.entity.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRoleRepository extends JpaRepository<UserRole, Integer> {
+    List<UserRole> findByUser(User user);
 }
