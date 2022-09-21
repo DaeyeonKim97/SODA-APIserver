8433cab66de2ad9f29639b9005e69d937af454c5
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-09-21 12:38:25 +0900

    fix: fix import directories

--- src/main/java/com/soda/apiserver/auth/controller/SignUpController.java
+++ src/main/java/com/soda/apiserver/auth/controller/SignUpController.java
@@ -7,7 +7,7 @@ import com.soda.apiserver.auth.model.entity.UserRole;
 import com.soda.apiserver.auth.repository.RoleRepository;
 import com.soda.apiserver.auth.repository.UserRepository;
 import com.soda.apiserver.auth.repository.UserRoleRepository;
-import com.soda.common.response.ResponseMessage;
+import com.soda.apiserver.common.response.ResponseMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
