b1341eece4158f5b2b434237ad0c407b2cf2ac22
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-09-21 22:05:53 +0900

    feature: change password dto

---
+++ src/main/java/com/soda/apiserver/auth/model/dto/ChangePasswordDTO.java
@@ -0,0 +1,38 @@
+package com.soda.apiserver.auth.model.dto;
+
+public class ChangePasswordDTO {
+    private String oldPassword;
+    private String newPassword;
+
+    public ChangePasswordDTO() {
+    }
+
+    public ChangePasswordDTO(String oldPassword, String newPassword) {
+        this.oldPassword = oldPassword;
+        this.newPassword = newPassword;
+    }
+
+    public String getOldPassword() {
+        return oldPassword;
+    }
+
+    public void setOldPassword(String oldPassword) {
+        this.oldPassword = oldPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+
+    @Override
+    public String toString() {
+        return "ChangePasswordDTO{" +
+                "oldPassword='" + oldPassword + '\'' +
+                ", newPassword='" + newPassword + '\'' +
+                '}';
+    }
+}
