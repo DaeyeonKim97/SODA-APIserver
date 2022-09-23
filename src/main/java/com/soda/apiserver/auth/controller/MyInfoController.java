01bffc56b493db7f34fc6fa6f593f9e10151d66f
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-09-23 16:14:29 +0900

    refactor: Change response type string to ResponseEntity

--- src/main/java/com/soda/apiserver/auth/controller/MyInfoController.java
+++ src/main/java/com/soda/apiserver/auth/controller/MyInfoController.java
@@ -3,11 +3,19 @@ package com.soda.apiserver.auth.controller;
 import com.soda.apiserver.auth.model.dto.ChangePasswordDTO;
 import com.soda.apiserver.auth.model.entity.User;
 import com.soda.apiserver.auth.service.MyInfoService;
+import com.soda.apiserver.common.response.ResponseMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/info")
@@ -21,43 +29,65 @@ public class MyInfoController {
 
     @ResponseBody
     @GetMapping
-    public Object getMyInfo(HttpServletResponse response){
+    public ResponseEntity<?> getMyInfo(HttpServletResponse response){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType("application","json", Charset.forName("UTF-8")));
+        Map<String,Object> responseMap = new HashMap<>();
         String username = null;
+
         try{
             username = SecurityContextHolder.getContext().getAuthentication().getName();
         } catch (Exception e){
-            response.setStatus(401);
-            return "invalid token";
+            System.out.println(e);
+            return ResponseEntity
+                    .badRequest()
+                    .build();
         }
 
         User myInfo = myInfoService.getMyInfo(username);
 
         if(myInfo == null){
             response.setStatus(401);
-            return "invalid token";
+            return ResponseEntity
+                    .badRequest()
+                    .build();
         }
 
-        return myInfo;
+        responseMap.put("user", myInfo);
+
+        return ResponseEntity
+                .ok()
+                .headers(headers)
+                .body(new ResponseMessage(200, "information quired",responseMap));
     }
 
     @PutMapping("password")
-    public String updateMyPassword(@RequestBody ChangePasswordDTO passwordRequest, HttpServletResponse response){
+    public ResponseEntity<?> updateMyPassword(@RequestBody ChangePasswordDTO passwordRequest, HttpServletResponse response){
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType("application","json", Charset.forName("UTF-8")));
         String username = null;
+
         try{
             username = SecurityContextHolder.getContext().getAuthentication().getName();
         } catch (Exception e){
-            response.setStatus(401);
-            return "invalid token";
+            return ResponseEntity
+                    .badRequest()
+                    .build();
         }
         boolean result = myInfoService.updateMyPassword(username,
                 passwordRequest.getOldPassword(), passwordRequest.getNewPassword());
 
         if(result){
-            return "password successfully updated";
+            return ResponseEntity
+                    .created(URI.create(""))
+                    .headers(headers)
+                    .body(new ResponseMessage(201, "password changed",null));
         }
         else {
-            response.setStatus(400);
-            return "password update failed";
+            return ResponseEntity
+                    .badRequest()
+                    .body(new ResponseMessage(400, "failed to change password",null));
         }
     }
 }
