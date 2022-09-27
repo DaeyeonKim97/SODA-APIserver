f79d69373a30277157e356263fb76c2ad4002245
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-09-27 21:24:31 +0900

    feature: implement review insert response Data transfer object

---
+++ src/main/java/com/soda/apiserver/review/model/dto/ReviewResponseDTO.java
@@ -0,0 +1,109 @@
+package com.soda.apiserver.review.model.dto;
+
+import com.soda.apiserver.auth.model.dto.OtherUserDTO;
+import com.soda.apiserver.wish.model.entity.Restaurant;
+
+import java.util.Date;
+
+public class ReviewResponseDTO {
+    private int id;
+    private OtherUserDTO user;
+    private String categoryName;
+    private Restaurant restaurant;
+    private String imageSrc;
+    private int grade;
+    private String content;
+    private Date createDate;
+
+    public ReviewResponseDTO() {
+    }
+
+    public ReviewResponseDTO(int id, OtherUserDTO user, String categoryName, Restaurant restaurant, String imageSrc, int grade, String content, Date createDate) {
+        this.id = id;
+        this.user = user;
+        this.categoryName = categoryName;
+        this.restaurant = restaurant;
+        this.imageSrc = imageSrc;
+        this.grade = grade;
+        this.content = content;
+        this.createDate = createDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public OtherUserDTO getUser() {
+        return user;
+    }
+
+    public void setUser(OtherUserDTO user) {
+        this.user = user;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getImageSrc() {
+        return imageSrc;
+    }
+
+    public void setImageSrc(String imageSrc) {
+        this.imageSrc = imageSrc;
+    }
+
+    @Override
+    public String toString() {
+        return "ReviewResponseDTO{" +
+                "id=" + id +
+                ", user=" + user +
+                ", categoryName='" + categoryName + '\'' +
+                ", restaurant=" + restaurant +
+                ", imageSrc='" + imageSrc + '\'' +
+                ", grade=" + grade +
+                ", content='" + content + '\'' +
+                ", createDate=" + createDate +
+                '}';
+    }
+}
